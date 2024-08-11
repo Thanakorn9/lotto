cd162ace74be845de275086dc78bbd53d8cf8ddf
Thanakorn9 <65011212060@msu.ac.th>
2024-08-11 12:25:28 +0700

    app.js

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
 const http = require('http');
+const port = process.env.PORT || 3000;
 
-const hostname = '127.0.0.1';
-
-const port = 5000;
-
-const server = http.createServer((req, res) =>{
-    res.statusCode = 200;
-    res.setHeader('Content-Type','text/plain');
-    res.end('Hello World\n');
-
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  const msg = 'Hello World!!\n'
+  res.end(msg);
 });
 
-server.listen(port,hostname,() =>{
-    console.log(`Server Running at http://${hostname}:${port}/`)
+server.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}/`);
 });
